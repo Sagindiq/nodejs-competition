b3dfa58b304154fdd908d2646db93960bb77e0be
Sag'indiq Begaydarov <98073575+Sagindiq@users.noreply.github.com>
2023-02-19 14:19:33 +0500

    environment config added

--- src/utils/mongoose.ts
+++ src/utils/mongoose.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import {} from '../model'
-const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/node-competition'
+import { MONGO_CONNECTION_STRING } from '../config'
 
 const mongo = async () => {
     return await mongoose.connect(MONGO_CONNECTION_STRING)
